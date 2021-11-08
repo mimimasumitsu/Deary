5a4e4f9ecae869e167750c68a62d1f9bd64ff94d
mimimasumitsu <mi.msmt.123@gmail.com>
2021-11-08 14:55:05 +0000

    [add]アバウトページ・[update]レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
+  get "home/about" => 'homes#about'
   resources :albums, only: [:index, :new, :create] do
     resources :photos, only: [:index, :new, :create, :show, :destroy] do
       resources :comments, only: [:create, :destroy]
